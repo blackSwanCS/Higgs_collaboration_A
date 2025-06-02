7f15bbf540551874c6cdc2383a0d5680042c252b
nicofighter45 <fagot.nicolas.45@gmail.com>
2025-06-02 14:33:02 +0200

    Comment BDT skeleton code

--- sample_code_submission/boosted_decision_tree.py
+++ sample_code_submission/boosted_decision_tree.py
@@ -4,23 +4,29 @@ from sklearn.preprocessing import StandardScaler
 
 class BoostedDecisionTree:
     """
-    This Dummy class implements a decision tree classifier
-    change the code in the fit method to implement a decision tree classifier
-
-
+    This class implements a boosted devision tree model
     """
 
     def __init__(self, train_data):
+        # Initialize the model and scaler
         self.model = XGBClassifier()
         self.scaler = StandardScaler()
 
     def fit(self, train_data, labels, weights=None):
 
+        # Set the scaler to look at the data
         self.scaler.fit_transform(train_data)
 
+        # Resize the data using the scaler (mean 0, std 1)
         X_train_data = self.scaler.transform(train_data)
+
+        # Fit the model to the training data
         self.model.fit(X_train_data, labels, weights)
 
     def predict(self, test_data):
+
+        # Resize the test data using the scaler
         test_data = self.scaler.transform(test_data)
+
+        # Predict the probabilities for the positive class
         return self.model.predict_proba(test_data)[:, 1]
