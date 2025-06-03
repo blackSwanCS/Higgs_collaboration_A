0d5355eceb702db5ff98bde1218ffdb73abf1f1e
Mininger Quentin <quentin.mininger@student-cs.fr>
2025-06-03 15:29:20 +0200

    adding csv

--- sample_code_submission/BDT/BDT_parameters_search.py
+++ sample_code_submission/BDT/BDT_parameters_search.py
@@ -1,8 +1,10 @@
+import csv
 import numpy as np
 from HiggsML.datasets import download_dataset
 from boosted_decision_tree import BoostedDecisionTree
 from itertools import product
 from tqdm import tqdm
+from time import time 
 
 def get_data():
     data = download_dataset("blackSwan_data")
@@ -23,7 +25,10 @@ def get_data():
 
 def evaluate_significance(params, train_data, train_labels, train_weights, val_data, val_labels, val_weights):
     model = BoostedDecisionTree(params)
+    start_time = time()
     model.fit(train_data, train_labels, train_weights)
+    end_time = time()
+    print(f"Fitting time: {end_time - start_time:.2f} seconds")
     model.predict(val_data, labels=val_labels, weights=val_weights) 
     significance = model.significance()
     return significance
@@ -35,22 +40,28 @@ if __name__ == "__main__":
     # Define parameter grid (edit these lists as needed)
     n_estimators_list = np.linspace(100, 510, 10, dtype=int)
     max_depth_list = np.arange(2, 10, 1)
+    eta_list = np.linspace(0.01, 0.2, 10)
+    subsample_list = np.linspace(0.5, 1.0, 10)
 
     param_grid = list(product(
         n_estimators_list,
         max_depth_list,
+        eta_list,
+        subsample_list
     ))
 
     best_significance = -np.inf
     best_params = None
-
-    for n_estimators, max_depth in tqdm(param_grid):
+    excel = np.zeros((len(n_estimators_list)*len(max_depth_list),6))
+    i=0
+    for n_estimators, max_depth, eta, subsample in tqdm(param_grid):
         params = {
             "n_estimators": n_estimators,
             "max_depth": max_depth,
             "max_leaves": 0,
+            "eta": eta,
+            "subsample": subsample,
             "objective": "binary:logistic",
-            "use_label_encoder": False,
             "eval_metric": "logloss"
         }
 #        print(f"Testing params: {params}")
@@ -59,6 +70,9 @@ if __name__ == "__main__":
             train_data, train_labels, train_weights,
             val_data, val_labels, val_weights
         )
+        t=time.time()
+        excel[i]=[n_estimators,max_depth,eta,subsample,t,significance]
+        i+=1
 #        print(f"Significance: {significance:.4f}")
         if significance > best_significance:
             best_significance = significance
@@ -67,3 +81,7 @@ if __name__ == "__main__":
     print("\nBest parameters:")
     print(best_params)
     print(f"Best significance: {best_significance:.4f}")
+    csv_file_path = 'C:/Users/Bibi/Documents/EI/donnees_excel.csv'
+    with open (csv_file_path,mode='w',newline='') as file:
+        writer = csv.writer(file)
+        writer.writerows(excel)
