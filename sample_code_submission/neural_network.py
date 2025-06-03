4066a8f056de3dc386b284e6d00867a939255be3
Bakkouch <marwan.bakkouch@student-cs.fr>
2025-06-03 23:34:57 +0200

    modif

--- sample_code_submission/neural_network.py
+++ sample_code_submission/neural_network.py
@@ -42,33 +42,3 @@ class NeuralNetwork:
         test_data = self.scaler.transform(test_data)
         return self.model.predict(test_data).flatten().ravel()
     
-
-    #Vérification : 
-    def significance(self, test_data, labels, weights=None):
-        def __amsasimov(s_in,b_in):
-            s=np.copy(s_in)
-            b=np.copy(b_in)
-            s=np.where( (b_in == 0) , 0., s_in)
-            b=np.where( (b_in == 0) , 1., b)
-            ams = np.sqrt(2*((s+b)*np.log(1+s/b)-s))
-            ams=np.where( (s < 0)  | (b < 0), np.nan, ams)
-            if np.isscalar(s_in):
-                return float(ams)
-            else:
-                return  ams
-        def __significance_vscore(y_true, y_score, sample_weight=None):
-            if sample_weight is None:
-                sample_weight = np.full(len(y_true), 1.)
-            bins = np.linspace(0, 1., 101)
-            s_hist, bin_edges = np.histogram(y_score[y_true == 1], bins=bins, weights=sample_weight[y_true == 1])
-            b_hist, bin_edges = np.histogram(y_score[y_true == 0], bins=bins, weights=sample_weight[y_true == 0])
-            s_cumul = np.cumsum(s_hist[::-1])[::-1]
-            b_cumul = np.cumsum(b_hist[::-1])[::-1]
-            significance=__amsasimov(s_cumul,b_cumul)
-            max_value = np.max(significance)
-            return significance
-        y_pred_xgb = self.predict(test_data)
-        vamsasimov_xgb=__significance_vscore(y_true=labels, y_score=y_pred_xgb, sample_weight=weights)
-        significance_xgb = np.max(vamsasimov_xgb)
-        return significance_xgb
-    
