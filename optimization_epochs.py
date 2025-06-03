12f0dd3c3ae4c0c2c86b5438cab51bb8cf3f4bbb
Bakkouch <marwan.bakkouch@student-cs.fr>
2025-06-03 13:59:32 +0200

    new optimization

--- optimization_epochs.py
+++ optimization_epochs.py
@@ -1,7 +1,16 @@
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense
-from sklearn.preprocessing import StandardScaler
 import numpy as np
+
+from HiggsML.ingestion import Ingestion
+from HiggsML.datasets import download_dataset
+
+data = download_dataset(
+    "blackSwan_data"
+)  # change to "blackSwan_data" for the actual data
+
+# load train set
+data.load_train_set()
+data_set = data.get_train_set()
+
 
 class NeuralNetwork:
     """
@@ -26,7 +35,7 @@ class NeuralNetwork:
         )
         #Simule la fonction de perte
         self.scaler = StandardScaler()
-
+        
     def fit(self, train_data, y_train, weights_train=None):
 
         self.scaler.fit_transform(train_data)
@@ -34,14 +43,6 @@ class NeuralNetwork:
         self.model.fit(
             X_train, y_train, sample_weight=weights_train, epochs=5, verbose=2
         )
-    #epochs = nombre d'entraînement qu'on va faire
-    #verbose = débugs, informations sur les entraînements, fonction de coût.
-
-    def predict(self, test_data):
-        test_data = self.scaler.transform(test_data)
-        return self.model.predict(test_data).flatten().ravel()
-    
-
     #Vérification : 
     def significance(self, test_data, labels, weights=None):
         def __amsasimov(s_in,b_in):
@@ -70,4 +71,64 @@ class NeuralNetwork:
         vamsasimov_xgb=__significance_vscore(y_true=labels, y_score=y_pred_xgb, sample_weight=weights)
         significance_xgb = np.max(vamsasimov_xgb)
         return significance_xgb
-    
+
+
+
+
+
+
+
+
+
+
+TEST_SETTINGS = {
+    "systematics": {  # Systematics to use
+        "tes": False,  # tau energy scale
+        "jes": False,  # jet energy scale
+        "soft_met": False,  # soft term in MET
+        "ttbar_scale": False,  # W boson scale factor
+        "diboson_scale": False,  # Diboson scale factor
+        "bkg_scale": False,  # Background scale factor
+    },
+    "num_pseudo_experiments": 25,  # Number of pseudo-experiments to run per set
+    "num_of_sets": 25,  # Number of sets of pseudo-experiments to run
+}
+
+RANDOM_SEED = 42
+
+test_settings = TEST_SETTINGS.copy()
+
+random_state = np.random.RandomState(RANDOM_SEED)
+test_settings["ground_truth_mus"] = (
+    random_state.uniform(0.1, 3, test_settings["num_of_sets"])
+).tolist()
+
+random_settings_file = os.path.join(output_dir, "test_settings.json")
+with open(random_settings_file, "w") as f:
+    json.dump(test_settings, f)
+
+
+
+
+
+L_epochs = np.linspace(2, 15, 14)
+def optimization(train_data): 
+    for k in range(L_epochs): 
+        ingestion = Ingestion(data)
+        # initialize submission
+        ingestion.init_submission(Model, "NN")
+        # fit submission
+        ingestion.fit_submission()
+        # load test set
+        data.load_test_set()
+        # predict submission
+        ingestion.predict_submission(test_settings)
+        ingestion.process_results_dict()
+        significance(data_set, )
+        
+        
+        
+        
+        
+        
+        
