3eb69e05f28edc4daf2c1d131a810c28f3d6b207
de Goede Mexy <mexy.degoede@student-cs.fr>
2025-06-04 15:37:43 +0200

    optimization batchsize

--- sample_code_submission/neural_network.py
+++ sample_code_submission/neural_network.py
@@ -16,21 +16,22 @@ class NeuralNetwork:
 
         n_dim = train_data.shape[1]
 
-        self.model.add(Dense(10, input_dim=n_dim, activation="relu"))
-        self.model.add(Dense(10, activation="relu"))
+        self.model.add(Dense(100, input_dim=n_dim, activation="relu"))
+        self.model.add(Dense(100, activation="relu"))
         self.model.add(Dense(1, activation="sigmoid"))
 
         self.model.compile(
             loss="binary_crossentropy", optimizer="adam", metrics=["accuracy"]
         )
         self.scaler = StandardScaler()
+        self.batchsize = 50
 
     def fit(self, train_data, y_train, weights_train=None):
         # Fit the scaler on the training data
         self.scaler.fit_transform(train_data)
         X_train = self.scaler.transform(train_data)
         # Fit the model using the transformed data
-        self.model.fit(X_train, y_train, sample_weight=weights_train, epochs=5, verbose=2)
+        self.model.fit(X_train, y_train, sample_weight=weights_train, epochs=5, batch_size = self.batchsize, verbose=2)
 
     def predict(self, test_data, labels=None, weights=None):
         test_data = self.scaler.transform(test_data)
