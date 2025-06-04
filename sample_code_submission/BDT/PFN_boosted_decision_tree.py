76b21996e433bb09a6dfd9e733daeed5b44a7d94
Roblot <koske.roblot@student-cs.fr>
2025-06-04 17:55:37 +0200

    TabPFN classifier test done and Black reformatting

--- sample_code_submission/BDT/PFN_boosted_decision_tree.py
+++ sample_code_submission/BDT/PFN_boosted_decision_tree.py
@@ -3,6 +3,7 @@ from tabpfn import TabPFNClassifier
 from get_data import get_data
 from time import time
 
+
 class PFN_boosted_decision_tree(AbstractBoostedDecisionTree):
     """
     This class implements a boosted decision tree model using PFN's implementation.
@@ -18,36 +19,39 @@ class PFN_boosted_decision_tree(AbstractBoostedDecisionTree):
         self._model.fit(
             self._scaler.transform(train_data),
             self._labels,
-#            sample_weight=self._weights,
+            #            sample_weight=self._weights,
         )
 
     def predict(self, test_data, labels=None, weights=None):
-        self._predicted_data = self.predict_full_output(test_data, labels, weights)[:, 1]
+        self._predicted_data = self.predict_full_output(test_data, labels, weights)[
+            :, 1
+        ]
         return self._predicted_data
 
     def predict_full_output(self, test_data, labels=None, weights=None):
         super().predict_full_output(test_data, labels, weights)
         return self._model.predict_proba(self._scaler.transform(test_data))
-    
+
     def load_model(self):
         return super().load_model()
-    
-    def save(self): 
+
+    def save(self):
         return super().save()
-    
+
+
 if __name__ == "__main__":
     train_data, train_labels, train_weights, val_data, val_labels, val_weights = (
-            get_data()
-        )
-    
-#    train_data = train_data[:1000]
-#    train_labels = train_labels[:1000]
-#    train_weights = train_weights[:1000]
-    
-#    val_data = val_data[:500]
-#    val_labels = val_labels[:500]
-#    val_weights = val_weights[:500]
-    
+        get_data()
+    )
+
+    train_data = train_data[:4000]
+    train_labels = train_labels[:4000]
+    train_weights = train_weights[:4000]
+
+    val_data = val_data[:2000]
+    val_labels = val_labels[:2000]
+    val_weights = val_weights[:2000]
+
     model = PFN_boosted_decision_tree()
     t0 = time()
     model.fit(train_data, train_labels, train_weights)
