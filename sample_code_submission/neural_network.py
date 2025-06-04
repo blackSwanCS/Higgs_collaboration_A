87e3f7daa7df5c4376e5ba0bcdd1305b5b4d6226
Bakkouch <marwan.bakkouch@student-cs.fr>
2025-06-04 22:52:15 +0200

    hey

--- sample_code_submission/neural_network.py
+++ sample_code_submission/neural_network.py
@@ -12,25 +12,26 @@ class NeuralNetwork:
     """
     def __init__(self, train_data):
         self.model = Sequential()
-
+        self.layers = 1
         n_dim = train_data.shape[1]
-
         self.model.add(Dense(100, input_dim=n_dim, activation="relu"))
-        self.model.add(Dense(100, activation="relu"))
-        self.model.add(Dense(1, activation="sigmoid"))
+        for k in range(self.layers): 
+            self.model.add(Dense(100, activation="relu"))
+            self.model.add(Dense(1, activation="sigmoid"))
 
         self.model.compile(
             loss="binary_crossentropy", optimizer="adam", metrics=["accuracy"]
         )
         self.scaler = StandardScaler()
         self.batchsize = 50
+        self.epochs = 5
 
     def fit(self, train_data, y_train, weights_train=None):
         # Fit the scaler on the training data
         self.scaler.fit_transform(train_data)
         X_train = self.scaler.transform(train_data)
         # Fit the model using the transformed data
-        self.model.fit(X_train, y_train, sample_weight=weights_train, epochs=5, batch_size = self.batchsize, verbose=2)
+        self.model.fit(X_train, y_train, sample_weight=weights_train, epochs= self.epochs, batch_size = self.batchsize, verbose=2)
 
     def predict(self, test_data, labels=None, weights=None):
         test_data = self.scaler.transform(test_data)
