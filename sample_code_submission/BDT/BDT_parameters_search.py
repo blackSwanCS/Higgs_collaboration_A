fbb39266bd739f18188c12d759ef6b77fc12fb2b
Julie Agin <julie.agin@student-cs.fr>
2025-06-04 16:29:20 +0200

    ajout affichage auc du modèle qui a la meilleure significance

--- sample_code_submission/BDT/BDT_parameters_search.py
+++ sample_code_submission/BDT/BDT_parameters_search.py
@@ -31,17 +31,18 @@ def evaluate_significance(params, train_data, train_labels, train_weights, val_d
     print(f"Fitting time: {end_time - start_time:.2f} seconds")
     model.predict(val_data, labels=val_labels, weights=val_weights) 
     significance = model.significance()
-    return significance
+    auc=model.auc()
+    return significance,auc
 
 if __name__ == "__main__":
     # Load data once
     train_data, train_labels, train_weights, val_data, val_labels, val_weights = get_data()
 
     # Define parameter grid (edit these lists as needed)
-    n_estimators_list = np.linspace(100, 510, 10, dtype=int)
+    n_estimators_list = np.linspace(500, 550, 1, dtype=int)
     max_depth_list = np.arange(2, 10, 1)
-    eta_list = np.linspace(0.01, 0.2, 10)
-    subsample_list = np.linspace(0.5, 1.0, 10)
+    eta_list = np.linspace(0.01, 0.2, 1)
+    subsample_list = np.linspace(0.75, 1.0, 1)
 
     param_grid = list(product(
         n_estimators_list,
@@ -51,8 +52,9 @@ if __name__ == "__main__":
     ))
 
     best_significance = -np.inf
+    auc_best_significance=0.5
     best_params = None
-    excel = np.zeros((len(n_estimators_list)*len(max_depth_list),6))
+    excel = np.zeros((len(n_estimators_list)*len(max_depth_list)*len(eta_list)*len(subsample_list),6))
     i=0
     for n_estimators, max_depth, eta, subsample in tqdm(param_grid):
         params = {
@@ -65,23 +67,24 @@ if __name__ == "__main__":
             "eval_metric": "logloss"
         }
 #        print(f"Testing params: {params}")
-        significance = evaluate_significance(
+        significance,auc = evaluate_significance(
             params,
             train_data, train_labels, train_weights,
             val_data, val_labels, val_weights
         )
-        t=time.time()
+        t=time()
         excel[i]=[n_estimators,max_depth,eta,subsample,t,significance]
         i+=1
 #        print(f"Significance: {significance:.4f}")
         if significance > best_significance:
-            best_significance = significance
+            best_significance,auc_best_significance = significance,auc
             best_params = params
 
     print("\nBest parameters:")
     print(best_params)
     print(f"Best significance: {best_significance:.4f}")
-    csv_file_path = 'C:/Users/Bibi/Documents/EI/donnees_excel.csv'
+    print(f"AUC du modèle avec la best significance: {auc_best_significance:.4f}")
+    csv_file_path = 'C:/Users/julie/Documents/CS/Cours/Black Swans/EI/donnees_500_550'
     with open (csv_file_path,mode='w',newline='') as file:
         writer = csv.writer(file)
         writer.writerows(excel)
--- sample_code_submission/BDT/boosted_decision_tree.py
+++ sample_code_submission/BDT/boosted_decision_tree.py
@@ -1,5 +1,6 @@
 from xgboost import XGBClassifier
 from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import roc_auc_score
 import numpy as np
 import multiprocessing
 from enum import Enum, auto
@@ -93,4 +94,7 @@ class BoostedDecisionTree:
             sample_weight=self.__test_weights
         )
         return np.max(vamsasimov_xgb)
+    
+    def auc(self):
+        return roc_auc_score(y_true=self.__test_labels, y_score=self.__predicted_data,sample_weight=self.__test_weights)
 
