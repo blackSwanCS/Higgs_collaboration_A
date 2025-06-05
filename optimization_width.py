e7886b92c0558a74d5e2af73d370ce8315ab7223
Vanderbeken Thomas <thomas.vanderbeken@student-cs.fr>
2025-06-05 10:16:12 +0200

    Ajout de optimization_width.py

---
+++ optimization_width.py
@@ -0,0 +1,106 @@
+import numpy as np
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from sklearn.preprocessing import StandardScaler
+
+from HiggsML.ingestion import Ingestion
+from HiggsML.datasets import download_dataset
+from sample_code_submission.neural_network import NeuralNetwork
+from sys import path
+import numpy as np
+import pandas as pd
+import json
+import matplotlib.pyplot as plt
+from itertools import product
+from numpy.random import RandomState
+import warnings
+import os
+import sys
+
+warnings.filterwarnings("ignore")
+
+data = download_dataset(
+    "blackSwan_data"
+)  # change to "blackSwan_data" for the actual data
+
+# load train set
+data.load_train_set()
+data_set = data.get_train_set()
+
+target = data_set["labels"]
+weights = data_set["weights"]
+detailed_label = data_set["detailed_labels"]
+keys = np.unique(detailed_label)
+
+root_dir = os.getcwd()
+print("Root directory is", root_dir)
+submission_dir = os.path.join(root_dir, "sample_code_submission")
+
+# The directory where results and other outputs from the participant's code will be written
+output_dir = os.path.join(root_dir, "sample_result_submission")
+
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
+
+path.append(submission_dir)
+from sample_code_submission.model import Model
+
+TEST_SETTINGS = {
+    "systematics": {  # Systematics to use
+        "tes": False,  # tau energy scale
+        "jes": False,  # jet energy scale
+        "soft_met": False,  # soft term in MET
+        "ttbar_scale": False,  # W boson scale factor
+        "diboson_scale": False,  # Diboson scale factor
+        "bkg_scale": False,  # Background scale factor
+    },
+    "num_pseudo_experiments": 25,  # Number of pseudo-experiments to run per set
+    "num_of_sets": 25,  # Number of sets of pseudo-experiments to run
+}
+
+RANDOM_SEED = 42
+
+test_settings = TEST_SETTINGS.copy()
+
+random_state = np.random.RandomState(RANDOM_SEED)
+test_settings["ground_truth_mus"] = (
+    random_state.uniform(0.1, 3, test_settings["num_of_sets"])
+).tolist()
+
+random_settings_file = os.path.join(output_dir, "test_settings.json")
+with open(random_settings_file, "w") as f:
+    json.dump(test_settings, f)
+
+def graphique(S, E):
+    plt.plot(S, E, marker='o', linestyle='-', color='b')
+    plt.title('Significance en fonction de width')
+    plt.ylabel('significance')
+    plt.xlabel('width')
+    plt.legend()
+    plt.grid(True)
+    plt.show()
+
+def optimization(train_data): 
+    L_width = np.linspace(10, 10000, 15)
+    sigmax = 0
+    width_max = 1
+    S = []
+    E = []
+    for k in range(len(L_width)):
+        ingestion = Ingestion(data)
+        # initialize submission
+        ingestion.init_submission(Model, "NN")
+        ingestion.model.model.width = int(L_width[k])
+        print(ingestion.model.model.width)
+        ingestion.fit_submission()
+        sig1 = ingestion.model.model.significance(test_labels=ingestion.model.training_set["labels"],test_weights=ingestion.model.training_set["weights"])
+        #visualisation des données :
+        S.append(sig1)
+        E.append(int(L_width[k]))
+        if sig1>sigmax: 
+            sigmax = sig1
+            width_max = int(L_width[k])
+    graphique(S, E)
+    return sigmax, width_max
+
+optimization(data_set)
