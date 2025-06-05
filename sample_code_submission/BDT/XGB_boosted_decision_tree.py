18e7f63cbbc1874cf8b5fb109761b0a9e7f3e24a
Koské Roblot <koske.roblot@student-cs.fr>
2025-06-05 11:40:46 +0200

    Update XGB_boosted_decision_tree.py

--- sample_code_submission/BDT/XGB_boosted_decision_tree.py
+++ sample_code_submission/BDT/XGB_boosted_decision_tree.py
@@ -3,10 +3,7 @@ from xgboost import XGBClassifier
 
 import os
 import sys
-
-
 def find_and_add_module_path(filename):
-    """Ajoute à sys.path le dossier contenant filename, en remontant jusqu'à 3 niveaux."""
     cur = os.path.abspath(os.path.dirname(__file__))
     for _ in range(3):
         candidate = os.path.join(cur, filename)
